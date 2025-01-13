9a49d953829a352c70d4d6cbdf293e7ecab64c1b
Gabriel Rocha <protonmail.default.sense07@protonmail.com>
2025-01-13 13:23:08 -0300

    new file:   src/prisma/migrations/20250113161536_db_teste_01_migrations/migration.sql 	modified:   src/prisma/schema.prisma 	modified:   src/prisma/seed.ts

--- src/prisma/seed.ts
+++ src/prisma/seed.ts
@@ -19,6 +19,15 @@ async function main() {
         foto_local:
           'https://www.creativefabrica.com/wp-content/uploads/2019/08/Restaurant-Logo-by-Koko-Store-580x386.jpg',
         promocao_rolando: true,
+        horario_de_funcionamento: JSON.stringify({
+          segunda: { abre: '08:00', fecha: '20:00' },
+          terca: { abre: '08:00', fecha: '20:00' },
+          quarta: { abre: '08:00', fecha: '20:00' },
+          quinta: { abre: '08:00', fecha: '20:00' },
+          sexta: { abre: '08:00', fecha: '22:00' },
+          sabado: { abre: '10:00', fecha: '22:00' },
+          domingo: { abre: '10:00', fecha: '18:00' },
+        }),
         fk_tipo_estabelecimento: 1,
       },
       {
@@ -30,29 +39,36 @@ async function main() {
         foto_local:
           'https://marketplace.canva.com/EAGPvH7XcNA/1/0/1600w/canva-logo-restaurante-delivery-rustico-Rvgeb9_mfLk.jpg',
         promocao_rolando: false,
+        horario_de_funcionamento: JSON.stringify({
+          segunda: { abre: '07:00', fecha: '22:00' },
+          terca: { abre: '07:00', fecha: '22:00' },
+          quarta: { abre: '07:00', fecha: '22:00' },
+          quinta: { abre: '07:00', fecha: '22:00' },
+          sexta: { abre: '07:00', fecha: '23:00' },
+          sabado: { abre: '07:00', fecha: '23:00' },
+          domingo: { abre: '08:00', fecha: '20:00' },
+        }),
         fk_tipo_estabelecimento: 2,
       },
       {
-        cnpj: '98765432000100',
+        cnpj: '45612378000123',
         endereco: 'Av. Horizontal, 789',
         nome: 'Loja de Roupas Central',
         aberto: true,
-        website: 'https://supercentral.com',
+        website: 'https://lojaroupascentral.com',
         foto_local:
           'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhCGZYpOBHyS6gQ5vuAu21l5nhk6kyjBsS7cIJcicU9_-Qv6W_kU7Voio1rQa3XFmNwsc&usqp=CAU',
         promocao_rolando: false,
-        fk_tipo_estabelecimento: 2,
-      },
-      {
-        cnpj: '98765432000100',
-        endereco: 'Av. Horizontal, 789',
-        nome: 'Loja de Roupas Central',
-        aberto: true,
-        website: 'https://supercentral.com',
-        foto_local:
-          'https://img.freepik.com/vector-premium/logotipo-restaurante-occidental-ilustracion-vector-personaje-dibujos-animados-chef_100311-7.jpg',
-        promocao_rolando: false,
-        fk_tipo_estabelecimento: 2,
+        horario_de_funcionamento: JSON.stringify({
+          segunda: { abre: '10:00', fecha: '19:00' },
+          terca: { abre: '10:00', fecha: '19:00' },
+          quarta: { abre: '10:00', fecha: '19:00' },
+          quinta: { abre: '10:00', fecha: '19:00' },
+          sexta: { abre: '10:00', fecha: '20:00' },
+          sabado: { abre: '10:00', fecha: '20:00' },
+          domingo: null, // Fechado no domingo
+        }),
+        fk_tipo_estabelecimento: 3,
       },
     ],
   });
