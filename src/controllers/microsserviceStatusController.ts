286955cd6effc8a65100384f3de2d28e1a4d7546
Gabriel Rocha <protonmail.default.sense07@protonmail.com>
2025-03-24 09:59:53 +0000

    Mar 24, 2025, 6:59 AM

--- src/controllers/microsserviceStatusController.ts
+++ src/controllers/microsserviceStatusController.ts
@@ -1,5 +1,7 @@
 import { FastifyReply, FastifyRequest } from 'fastify';
 
 export async function microsserviceStatusController(req: FastifyRequest, reply: FastifyReply) {
-  return reply.status(200).send({ status: 'The microservice is running' });
+  return reply
+    .status(200)
+    .send({ status: 'The microservice is running', version: '1.0.0', service_name: 'ms_estabelecimentos' });
 }
