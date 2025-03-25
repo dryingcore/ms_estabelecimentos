545f4e4d9398afc18c22a4cea8623fc5626bbec2
Gabriel Rocha <protonmail.default.sense07@protonmail.com>
2025-03-25 16:32:53 +0000

    Mar 25, 2025, 1:32 PM

--- src/controllers/microsserviceStatusController.ts
+++ src/controllers/microsserviceStatusController.ts
@@ -1,7 +1,19 @@
 import { FastifyReply, FastifyRequest } from 'fastify';
+import { readFileSync } from 'fs';
+import { join } from 'path';
+
+const packageJson = JSON.parse(readFileSync(join(__dirname, '../package.json'), 'utf-8'));
 
 export async function microsserviceStatusController(req: FastifyRequest, reply: FastifyReply) {
-  return reply
-    .status(200)
-    .send({ status: 'The microservice is running', version: '1.0.0', service_name: 'ms_estabelecimentos' });
+  return reply.status(200).send({
+    status: 'The microservice is running',
+    version: packageJson.version,
+    service_name: 'ms_estabelecimentos',
+    date: new Date(),
+    endpoints: [
+      { method: 'GET', path: '/estabelecimentos', description: 'Listar estabelecimentos' },
+      { method: 'GET', path: '/estabelecimentos/tipos', description: 'Listar tipos de estabelecimentos' },
+      { method: 'POST', path: '/estabelecimentos', description: 'Criar estabelecimento' },
+    ],
+  });
 }
